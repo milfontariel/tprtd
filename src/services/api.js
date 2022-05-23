01047a0611b440d1ea78bd9de3108afc9d491df5
ariel <ariel1242009@gmail.com>
2022-05-23 16:31:46 -0300

    fix api base url

--- src/services/api.js
+++ src/services/api.js
@@ -4,35 +4,35 @@ axios.defaults.withCredentials = true;
 const { VITE_BASE_URL } = import.meta.env;
 
 export async function getProviders() {
-    const response = await axios.get(`${BASE_URL}/providers`);
+    const response = await axios.get(`${VITE_BASE_URL}/providers`);
     return response.data;
 }
 
 export async function getGenres() {
-    const response = await axios.get(`${BASE_URL}/genres`)
+    const response = await axios.get(`${VITE_BASE_URL}/genres`)
     return response.data;
 }
 
 export async function postSelectedProviders(selectedProviders) {
-    return await axios.post(`${BASE_URL}/providers`, selectedProviders, {});
+    return await axios.post(`${VITE_BASE_URL}/providers`, selectedProviders, {});
 }
 
 export async function getRecommendedMovies(page, genre, watchProvider) {
-    const response = await axios.get(`${BASE_URL}/recommended/${page}?genre=${genre}&watch_provider=${watchProvider}`);
+    const response = await axios.get(`${VITE_BASE_URL}/recommended/${page}?genre=${genre}&watch_provider=${watchProvider}`);
     return response.data;
 }
 
 export async function getMovieDetails(id) {
-    const response = await axios.get(`${BASE_URL}/movie/${id}`);
+    const response = await axios.get(`${VITE_BASE_URL}/movie/${id}`);
     return response.data;
 }
 
 export async function getMoviesByProvider(providerId) {
-    const response = await axios.get(`${BASE_URL}/movies/${providerId}`);
+    const response = await axios.get(`${VITE_BASE_URL}/movies/${providerId}`);
     return response.data;
 }
 
 export async function getMoviesByGenre(genreId) {
-    const response = await axios.get(`${BASE_URL}/movies/genre/${genreId}`)
+    const response = await axios.get(`${VITE_BASE_URL}/movies/genre/${genreId}`)
     return response.data;
 }
